46c9eee366bbf0436a12be3b927dc08d13a8e23d
HEKEPOIU <decnovpk92805@gmail.com>
2023-05-02 04:05:10 +0800

    UI Base Logic End.

--- Assets/Script/WebManerger.cs
+++ Assets/Script/WebManerger.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -8,7 +9,7 @@ public class WebManerger : MonoBehaviour
 {
     static WebManerger instance;
     [SerializeField] string serverIP; //wait for change to socket.
-    [SerializeField] List<string> tapoIP;
+    [SerializeField] ObservableCollection<string> tapoIP;
     public string Email { get; set; }
     public string Password { get; set; }
 
@@ -20,6 +21,7 @@ public class WebManerger : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
         else Destroy(gameObject);
+
     }
     public static WebManerger Instance
     {
@@ -32,7 +34,7 @@ public class WebManerger : MonoBehaviour
             return instance;
         }
     }
-    public List<string> TapoIP { get {  return tapoIP; } set {  tapoIP = value; } }
+    public ObservableCollection<string> TapoIP { get {  return tapoIP; } set {  tapoIP = value; } }
     public string ServerIP { get { return serverIP; } set { serverIP = value; } }
 
     //取代協成的c#預設寫法,由於他可以回傳東西，所以我用這個。
@@ -64,6 +66,34 @@ public class WebManerger : MonoBehaviour
 
 
     }
+    public async Task<string> SwitchPlugin(int which, bool isOn)
+    {
+        string _whatTodo = isOn ? "On" : "Off";
 
+        UnityWebRequest request = UnityWebRequest.Get(serverIP + "/" + Email + "/" + Password + "/" + tapoIP[which] + "/" + _whatTodo);
+
+        request.SetRequestHeader("ngrok-skip-browser-warning", "1");
+
+
+        request.SendWebRequest();
+
+        while (!request.isDone)
+        {
+            Debug.Log("執行中");
+            await Task.Yield();//這裡純等待執行完畢。
+
+        }
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            return request.downloadHandler.text;
+        }
+        else
+        {
+            return "Error";
+        }
+
+
+    }
 
 }
